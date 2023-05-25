0b64173ff2edfca46e3fad1f0a3d74cf6004eade
sairachapudi28 <52847780+sairachapudi28@users.noreply.github.com>
2023-05-25 11:19:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,5 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-@RestController
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, Docker!";
+        System.out.println("Hello, World!");
     }
 }
